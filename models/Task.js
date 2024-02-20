fa22f9b3f03e013b5fc9985772f5956e23d19f74
Usman Nurudeen <usmannurudeen13@gmail.com>
2024-02-20 07:04:29 +0100

    first commit

---
+++ models/Task.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true,
+    maxLength: [20, "name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
